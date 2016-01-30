36e17aabc806cbecb2da235cdbad88fd44480849
Victor Vasquez <victor.vasquez96@unitec.edu>
2016-01-29 23:30:32 -0600

    Update

--- Array/Array/Array.cpp
+++ Array/Array/Array.cpp
@@ -4,46 +4,62 @@
 #include "stdafx.h"
 
 using namespace std;
-int *numeros;
 
-	void inicio(){
-		numeros = new int[5];
-		for (int i = 0; i < 5; i++)
-			numeros[i] = i;
-	}
-
-	void buscar(int n){
-		for (int i = 0; i < sizeof(numeros) / sizeof(numeros[0]); i++){
-			if (numeros[i] == n){
-				cout << "Valor encontrado" << endl;
-				cout << "" << endl;
-			}
+int buscar(int valor, int arreglo[], int tam){
+	for (int i = 0; i < tam; i++){
+		if (arreglo[i] == valor){
+			return i;
 		}
 	}
+	return -1;
+}
+void agregar(int arreglo[], int valor, int tam){
+	int a = buscar(-1, arreglo, tam);
+	if (a != -1)
+		arreglo[a] = valor;
+}
 
-	void agregar(int num){
-		int* temp = new int[6];
-		std::copy(numeros, numeros + num, temp);
-		delete[] numeros;
-		numeros = temp;
+void insertar(int arreglo[], int valor, int pos, int tam){
+	int a = arreglo[pos];
+	int b;
+	arreglo[pos] = valor;
+	while (pos<tam){
+		b = arreglo[pos + 1];
+		arreglo[pos + 1] = a;
+		a = b;
+		pos++;
 	}
+}
 
-	void eliminar(int a){
-
+void borrar(int valor, int arreglo[], int tam){
+	int d = buscar(valor, arreglo, tam);
+	if (d != -1){
+		arreglo[d] = -1;
 	}
+}
 
-	void print(){
-		for (int a = 0; a < sizeof(numeros) / sizeof(numeros[0]); a++){
-			cout << numeros[a] << endl;
-		}
+void printArreglo(int arreglo[], int tam){
+	for (int x = 0; x < tam; x++){
+		cout << arreglo[x] << endl;
 	}
+}
+
+
 
 int main()
 {
-	inicio();
-	buscar(5);
-	eliminar(7);
-	print();
+	cout << "Empesando Codigo :v" << endl;
+	cout << "" << endl;
+
+	int arreglo[5] = {1,2,3,4,5};
+	int tam = sizeof(arreglo) / sizeof(arreglo[0]);
+
+	agregar(arreglo, 45, tam);
+	borrar(5, arreglo, tam);
+	insertar(arreglo, 19, 3, tam);
+	printArreglo(arreglo, tam);
+	cout << "" << endl;
+	cout << "Terminando codigo :,(" << endl;
 	return 0;
 }
 
